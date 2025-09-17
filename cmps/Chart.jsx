59067274547c4f4da74a056aeb55a92e9cc45597
veritas <liorgby2@gmail.com>
2025-09-18 01:37:51 +0300

    i'm batman

---
+++ cmps/Chart.jsx
@@ -0,0 +1,13 @@
+export function Chart({ data }) {
+  return (
+    <ul className="chart">
+      {data.map((item) => (
+        <li key={item.title}>
+          <span title={item.title} style={{ height: item.value + "%" }}>
+            {item.value + "%"}
+          </span>
+        </li>
+      ))}
+    </ul>
+  );
+}
