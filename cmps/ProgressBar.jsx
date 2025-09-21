767eebc57fa00bbb78c3fc14af3f24342f3785bc
veritas <liorgby2@gmail.com>
2025-09-21 08:47:19 +0300

    adding favicon + fixing NaN progress bar warning

--- cmps/ProgressBar.jsx
+++ cmps/ProgressBar.jsx
@@ -4,11 +4,19 @@ export function ProgressBar() {
   const todoList = useSelector((state) => state.todoModule.todos);
   const doneTodos = todoList.filter((todo) => todo.isDone).length;
   const percentage = 100 * (doneTodos / todoList.length);
-  const truncatedPercentage = Math.round(100 * percentage) / 100;
+  let truncatedPercentage = Math.round(100 * percentage) / 100;
+  let progValue;
+  if (isNaN(truncatedPercentage)) {
+    truncatedPercentage = String(NaN);
+    progValue = String(NaN);
+  } else {
+    progValue = truncatedPercentage / 100
+  }
+  
   return (
     <div className="progress-container">
       <h1>progress: {truncatedPercentage}%</h1>
-      <progress value={truncatedPercentage/100}></progress>
+      <progress value={progValue}></progress>
     </div>
   );
 }
--- store/store.js
+++ store/store.js
@@ -14,7 +14,3 @@ const rootReducer = combineReducers({
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const store = createStore(rootReducer, composeEnhancers());
 window.gStore = store;
-
-store.subscribe(() => {
-  console.log("Current state is:", store.getState());
-});
